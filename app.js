b228f047b410d1b1f43494bd1e7e270e5780eea8
Geoffery <iterationplus@gmail.com>
2023-02-04 11:33:37 +0100

    variable port

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app.get('/', (req,res)=>{
     res.send("Hey there! Welcome to the Verve.!");
 });
 
-
-app.listen(3000,()=>{
+const port = process.env.port || 3000;
+app.listen(port,()=>{
     console.log("This is Great! Our App is listing to port: ");
 });
 
