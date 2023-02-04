5194e0c856a54560e3db7243c39614230c9dac02
Geoffery <iterationplus@gmail.com>
2023-02-04 12:54:32 +0100

    added 2 roues

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ app.get('/', (req,res)=>{
     res.send("Hey there! Welcome to the Verve.! And i just added this codes");
 });
 
+app.get('/about',(req,res)=>{
+     res.send("This is the about page...");
+});
+
+
+app.get('/api',(req,res)=>{
+    res.json({message:"Good"});
+});
+
 const port = process.env.port || 3000;
 app.listen(port,()=>{
     console.log("This is Great! Our App is listing to port:", port);
